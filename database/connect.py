f934d58bedcad5f85a2d3792526aba44be7f0103
tzevi stern <z02072000@gmail.com>
2024-10-10 11:40:45 +0300

    add the database init

--- database/connect.py
+++ database/connect.py
@@ -1,10 +1,8 @@
 from pymongo import MongoClient
 
-
 client = MongoClient('mongodb://localhost:27017')
 
 # the data
 
-# taxi_db = client['taxi-drivers']
-# drivers = taxi_db['drivers']
-# cars = taxi_db['cars']
+Chicago_accidents_db = client['Chicago_accidents']
+accidents = Chicago_accidents_db['accidents']
---
+++ repository/csv_repository.py
@@ -0,0 +1,33 @@
+import csv
+from database.connect import accidents
+
+
+def read_csv(csv_path):
+    with open(csv_path, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            yield row
+
+
+def init_accidents():
+    accidents.drop()
+
+    for row in read_csv('../data/Traffic_Crashes_-_Crashes - 20k rows.csv'):
+        crash_date = {
+            'foll_date': row['CRASH_DATE'],
+            'day_of_week': row['CRASH_DAY_OF_WEEK'],
+        }
+        injuries = {
+            'total': row['INJURIES_TOTAL'],
+            'fatal': row['INJURIES_FATAL'],
+        }
+
+        accident = {
+            'crash_record_id': row['CRASH_RECORD_ID'],
+            'beat_of_occurrence': row['BEAT_OF_OCCURRENCE'],
+            'contributory_cause': row['PRIM_CONTRIBUTORY_CAUSE'],
+            'crash_date': crash_date,
+            'injuries': injuries
+        }
+
+        accidents.insert_one(accident)
